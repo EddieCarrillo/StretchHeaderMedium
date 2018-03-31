68ae571803c861f71c81a2c91aa45a2e6f69951f
Eduardo Carrillo <edcarril@ucsd.edu>
2018-03-31 08:16:12 -0700

    Initial Commit

---
+++ StretchyEffect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StretchyEffect
+//
+//  Created by Eduardo Carrillo on 3/31/18.
+//  Copyright © 2018 Eduardo Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

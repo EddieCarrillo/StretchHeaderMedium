455e082ec8efe03711687da02d6167843eb567df
Eduardo Carrillo <edcarril@ucsd.edu>
2018-04-01 09:14:25 -0700

    Added image

--- StretchyEffect/ViewController.swift
+++ StretchyEffect/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageviewHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var imageviewTop: NSLayoutConstraint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

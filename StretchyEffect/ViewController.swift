3fc853d12e981a564adb21149fd2937da2e71616
Eduardo Carrillo <edcarril@ucsd.edu>
2018-06-22 21:50:23 -0700

    Added part 2 implementation to this project (Non tableview imp.)

--- StretchyEffect/ViewController.swift
+++ StretchyEffect/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var scrollView: UIScrollView!
     
+    
+    var stickyHeight: CGFloat!
+    
     var originalHeight: CGFloat!
     
     
@@ -23,6 +26,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         scrollView.delegate = self
         originalHeight = imageviewHeight.constant
+        
+        //Make the height of sticky image fraction of image
+        //This sticky height is arbitrary you can make it whatever you feel like.
+        let scaleFactor = CGFloat(2)
+        
+        //Make the height of the sticky part 'one eighth' of the size of the image
+        stickyHeight = imageviewHeight.constant / scaleFactor
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,18 +48,32 @@ extension ViewController: UIScrollViewDelegate{
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let offset = scrollView.contentOffset.y
         
+        let stickyThreshold = originalHeight - stickyHeight
+        
         let defaultTop: CGFloat = CGFloat(0)
         
         var currentTop: CGFloat = defaultTop
+        print("offset: \(offset)")
         
         if offset < 0{
             currentTop = offset
             imageviewHeight.constant = originalHeight - offset
         }else{
+            if (offset > stickyThreshold){ //Once only bottom 'one eighth of image shows....
+                //Keep pushing the image down to give illusion of stickiness
+                //How much do we push down?
+                //The amount we traveled relative to point that maintains our scaled background imageview height
+                currentTop = offset - stickyThreshold //
+                
+            }else {
+                //If we don't go bottom 1/scaleFactor is not showing exclusively then dont move image down.
+            }
             imageviewHeight.constant = originalHeight
         }
         
         imageviewTop.constant = currentTop
+
+        
     }
 }
 
